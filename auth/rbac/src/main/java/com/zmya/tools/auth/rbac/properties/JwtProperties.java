f848270e913fd40dff7845b18f93bc91a671df7d
precioussilence <deepautumnfeelings@gmail.com>
2025-07-26 17:00:13 +0800

    integrate jwt

---
+++ auth/rbac/src/main/java/com/zmya/tools/auth/rbac/properties/JwtProperties.java
@@ -0,0 +1,22 @@
+package com.zmya.tools.auth.rbac.properties;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Data
+@Validated
+public class JwtProperties {
+
+    @NotEmpty
+    private String secret;
+
+    @NotNull
+    private Integer expirationInSeconds;
+
+}
---
+++ auth/rbac/src/main/java/com/zmya/tools/auth/rbac/utils/JwtUtils.java
@@ -0,0 +1,43 @@
+package com.zmya.tools.auth.rbac.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtils {
+
+    public static String generateJwt(String username, Integer expirationInSeconds, String secret) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationInSeconds * 1000))
+                .signWith(generateSigningKey(secret), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public static String extractUsernameFromToken(String token, String secret) {
+        Claims body = Jwts.parserBuilder()
+                .setSigningKey(generateSigningKey(secret))
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+        if (body.getExpiration().after(new Date())) {
+            return null;
+        } else {
+            return body.getSubject();
+        }
+    }
+
+    private static Key generateSigningKey(String secret) {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private JwtUtils() {
+    }
+
+}
