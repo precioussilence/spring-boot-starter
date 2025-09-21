d748a48f3c8d922061217974256a173633796e5e
precioussilence <deepautumnfeelings@gmail.com>
2025-09-21 22:09:33 +0800

    modify usage example

--- demo/src/main/java/com/zmya/demo/DemoApplication.java
+++ demo/src/main/java/com/zmya/demo/DemoApplication.java
@@ -2,12 +2,8 @@ package com.zmya.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.zmya.demo", "com.zmya.tools.*"})
-@EnableJpaRepositories(basePackages = {"com.zmya.tools.auth.rbac.repository"})
-@EntityScan(basePackages = {"com.zmya.tools.auth.rbac.entity"})
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
