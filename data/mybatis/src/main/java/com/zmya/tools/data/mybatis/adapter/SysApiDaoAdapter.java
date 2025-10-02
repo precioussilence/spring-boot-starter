9f5235916161b029d4697eeefa860fb57713d634
precioussilence <deepautumnfeelings@gmail.com>
2025-10-02 15:36:29 +0800

    populate SysApiDaoAdapter

--- data/mybatis/src/main/java/com/zmya/tools/data/mybatis/adapter/SysApiDaoAdapter.java
+++ data/mybatis/src/main/java/com/zmya/tools/data/mybatis/adapter/SysApiDaoAdapter.java
@@ -1,7 +1,128 @@
 package com.zmya.tools.data.mybatis.adapter;
 
+import com.zmya.tools.data.core.dao.SysApiDao;
+import com.zmya.tools.data.core.model.SysApi;
+import com.zmya.tools.data.mybatis.entity.SysApiEntity;
+import com.zmya.tools.data.mybatis.entity.SysRoleEntity;
+import com.zmya.tools.data.mybatis.mapper.*;
+import lombok.AllArgsConstructor;
+import org.mybatis.dynamic.sql.SqlBuilder;
+import org.mybatis.dynamic.sql.render.RenderingStrategies;
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.List;
 
 @Component
-public class SysApiDaoAdapter {
+@ConditionalOnProperty(name = "zmya.data.impl", havingValue = "mybatis", matchIfMissing = true)
+@AllArgsConstructor
+public class SysApiDaoAdapter implements SysApiDao {
+
+    private final SysApiMapper sysApiMapper;
+    private final SysRoleMapper sysRoleMapper;
+
+    @Override
+    public List<String> findRequiredRoleCodes(String url, String method) {
+        if (!StringUtils.hasText(url) && !StringUtils.hasText(method)) {
+            return List.of();
+        }
+        SelectStatementProvider provider = SqlBuilder
+                .select(SysRoleEntityDynamicSqlSupport.roleCode)
+                .from(SysApiEntityDynamicSqlSupport.sysApiEntity)
+                .join(SysResourceApiEntityDynamicSqlSupport.sysResourceApiEntity)
+                .on(SysApiEntityDynamicSqlSupport.id, SqlBuilder.equalTo(SysResourceApiEntityDynamicSqlSupport.apiId))
+                .join(SysResourceEntityDynamicSqlSupport.sysResourceEntity)
+                .on(SysResourceApiEntityDynamicSqlSupport.resourceId, SqlBuilder.equalTo(SysResourceEntityDynamicSqlSupport.id))
+                .join(SysRoleResourceEntityDynamicSqlSupport.sysRoleResourceEntity)
+                .on(SysResourceEntityDynamicSqlSupport.id, SqlBuilder.equalTo(SysRoleResourceEntityDynamicSqlSupport.resourceId))
+                .join(SysRoleEntityDynamicSqlSupport.sysRoleEntity)
+                .on(SysRoleResourceEntityDynamicSqlSupport.roleId, SqlBuilder.equalTo(SysRoleEntityDynamicSqlSupport.id))
+                .where(SysApiEntityDynamicSqlSupport.url, SqlBuilder.isEqualToWhenPresent(url))
+                .and(SysApiEntityDynamicSqlSupport.method, SqlBuilder.isEqualToWhenPresent(method))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        List<SysRoleEntity> entities = sysRoleMapper.selectMany(provider);
+        return entities.stream().map(SysRoleEntity::getRoleCode).toList();
+    }
+
+    @Override
+    public List<SysApi> findByIdIn(Collection<Long> ids) {
+        if (CollectionUtils.isEmpty(ids)) {
+            return List.of();
+        }
+        SelectStatementProvider provider = SqlBuilder
+                .select(SysApiEntityDynamicSqlSupport.sysApiEntity.allColumns())
+                .from(SysApiEntityDynamicSqlSupport.sysApiEntity)
+                .where(SysApiEntityDynamicSqlSupport.id, SqlBuilder.isIn(ids))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        List<SysApiEntity> entities = sysApiMapper.selectMany(provider);
+        return entities.stream().map(source -> {
+            SysApi target = new SysApi();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        }).toList();
+    }
+
+    @Override
+    public List<SysApi> findByUrlAndMethod(String url, String method) {
+        if (!StringUtils.hasText(url) && !StringUtils.hasText(method)) {
+            return List.of();
+        }
+        SelectStatementProvider provider = SqlBuilder
+                .select(SysApiEntityDynamicSqlSupport.sysApiEntity.allColumns())
+                .from(SysApiEntityDynamicSqlSupport.sysApiEntity)
+                .where(SysApiEntityDynamicSqlSupport.url, SqlBuilder.isEqualToWhenPresent(url))
+                .and(SysApiEntityDynamicSqlSupport.method, SqlBuilder.isEqualToWhenPresent(method))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        List<SysApiEntity> entities = sysApiMapper.selectMany(provider);
+        return entities.stream().map(source -> {
+            SysApi target = new SysApi();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        }).toList();
+    }
+
+    @Override
+    public List<SysApi> findByApiNameAndUrl(String apiName, String url) {
+        if (!StringUtils.hasText(apiName) && !StringUtils.hasText(url)) {
+            return List.of();
+        }
+        SelectStatementProvider provider = SqlBuilder
+                .select(SysApiEntityDynamicSqlSupport.sysApiEntity.allColumns())
+                .from(SysApiEntityDynamicSqlSupport.sysApiEntity)
+                .where(SysApiEntityDynamicSqlSupport.apiName, SqlBuilder.isEqualToWhenPresent(apiName))
+                .and(SysApiEntityDynamicSqlSupport.url, SqlBuilder.isEqualToWhenPresent(url))
+                .build()
+                .render(RenderingStrategies.MYBATIS3);
+        List<SysApiEntity> entities = sysApiMapper.selectMany(provider);
+        return entities.stream().map(source -> {
+            SysApi target = new SysApi();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        }).toList();
+    }
+
+    @Override
+    @Transactional
+    public List<SysApi> saveAll(Collection<SysApi> sysApis) {
+        List<SysApiEntity> entities = sysApis.stream().map(source -> {
+            SysApiEntity target = new SysApiEntity();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        }).toList();
+        entities.forEach(sysApiMapper::insert);
+        return entities.stream().map(source -> {
+            SysApi target = new SysApi();
+            BeanUtils.copyProperties(source, target);
+            return target;
+        }).toList();
+    }
 }
