00cbda1d64435bffcfd8cc5ca384085d401131d6
precioussilence <deepautumnfeelings@gmail.com>
2025-10-15 20:14:08 +0800

    add integration module

---
+++ integration/src/main/java/com/zmya/tools/App.java
@@ -0,0 +1,13 @@
+package com.zmya.tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
